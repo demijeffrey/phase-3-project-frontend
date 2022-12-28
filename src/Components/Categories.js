9f28430f66a3814c44e12962a71b73354a7b8ed4
Demaris Ashmore <demiashmore@yahoo.com>
2022-12-27 18:41:20 -0700

    Made day buttons functional

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,20 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import NavLinks from "./NavLinks"
 import TaskCard from "./TaskCard"
 
 function Categories({ days, tasks }) {
+
+    const [day, setDay] = useState('')
+    const [dayTasks, setDayTasks] = useState('')
+
+    function handleClick(day) {
+        // console.log(day)
+        setDay(day)
+    }
+    // console.log(day)
+
     return(
         <div className="row">
 
             <div className="col s3 grey">
                 {days.map(day => {
-                    return <NavLinks day={day} />
+                    return <NavLinks day={day} handleClick={handleClick} />
                 })}
             </div>
 
-            <div className="col s9 teal">
+            {/* <div className="col s9 teal">
                 {tasks.map(task => {
                     return <TaskCard task={task} />
+                })}
+            </div> */}
+            <div className="col s9 teal">
+                {tasks.map(task => {
+                    if(task.day_id === day.id){
+                        return <TaskCard task={task} />
+                    }
                 })}
             </div>
 
--- src/Components/NavLinks.js
+++ src/Components/NavLinks.js
@@ -1,13 +1,13 @@
 import React from 'react'
 // import {NavLink} from 'react-router-dom'
 
-function NavLinks( {day} ) {
+function NavLinks({ day, handleClick }) {
     return(
-        <nav className="navbar-fixed">
-            {/* <a className="waves-effect waves-light btn fixed">{day.name}</a> */}
-            <h5>{day.name}</h5>
-            <br /><br /><br />
-        </nav>
+        <div className="grey">
+            <a className="waves-effect waves-light btn fixed" onClick={() => handleClick(day)}>{day.name}</a>
+            {/* <h5>{day.name}</h5> */}
+            <br /><br />
+        </div>
     )
 }
 
