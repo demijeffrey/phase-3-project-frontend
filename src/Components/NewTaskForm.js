323140c5e5f59e390153555438c504c161c4b3a4
Demaris Ashmore <demiashmore@yahoo.com>
2022-12-27 18:59:13 -0700

    Created form

--- src/Components/NewTaskForm.js
+++ src/Components/NewTaskForm.js
@@ -1,8 +1,21 @@
 function NewTaskForm() {
     return (
-        <div>
-            
-        </div>
+        <form className="center">
+            <textarea className="white" type="" placeholder="new task here" />
+            <br />
+            <select class="browser-default">
+                <option value="" disabled selected>Select Day</option>
+                <option value="1">Sunday</option>
+                <option value="2">Monday</option>
+                <option value="3">Tuesday</option>
+                <option value="4">Wednesday</option>
+                <option value="5">Thursday</option>
+                <option value="6">Friday</option>
+                <option value="7">Saturday</option>
+            </select>
+            <br />
+            <input type="submit" />
+        </form>
     )
 }
 
