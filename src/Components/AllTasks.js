b9eae9e4f42aff24ce289151966823bd18ef2547
Demaris Ashmore <demiashmore@yahoo.com>
2022-12-29 11:41:09 -0700

    Sorted all tasks by day

--- src/Components/AllTasks.js
+++ src/Components/AllTasks.js
@@ -2,13 +2,16 @@ import TaskCard from "./TaskCard"
 
 function AllTasks({ tasks, removeTask }) {
 
+    const displayTasks = tasks.sort((a, b) => (a.day_id > b.day_id) ? 1 : -1)
+    // console.log(displayTasks)
+
     return(
         <div>
             <div className="teal">
                 <h3 className="center grey-text text-darken-4">All Tasks This Week</h3>
             </div>
             <div className="">
-                {tasks.map(task => {
+                {displayTasks.map(task => {
                     return <TaskCard task={task} removeTask={removeTask} />
                 })}
             </div>
