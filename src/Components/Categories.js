95a786e08440a56b8b482a4746985b2fc43e405f
Demaris Ashmore <demiashmore@yahoo.com>
2022-12-29 12:26:23 -0700

    Added header to tasks by day

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -5,10 +5,11 @@ import TaskCard from "./TaskCard"
 function Categories({ days, tasks, removeTask, updateTask }) {
 
     const [day, setDay] = useState('')
-    const [dayTasks, setDayTasks] = useState('')
+    const [isTrue, setIsTrue] = useState(true)
 
     function handleClick(day) {
         // console.log(day)
+        setIsTrue(false)
         setDay(day)
     }
     // console.log(day)
@@ -22,6 +23,7 @@ function Categories({ days, tasks, removeTask, updateTask }) {
                 })}
             </div>
             <div className="col s10 teal">
+                {isTrue ? <h4 className="center">A daily task planner to keep all your ducks in a row!</h4> : null}
                 {tasks.map(task => {
                     if(task.day_id === day.id){
                         return <TaskCard key={task.id} task={task} removeTask={removeTask} updateTask={updateTask} />
