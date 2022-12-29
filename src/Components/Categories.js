8d344b0a64d133bcb6b302111c1a1031b4ef1892
Demaris Ashmore <demiashmore@yahoo.com>
2022-12-29 12:34:37 -0700

    Updated task header and header CSS

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -23,7 +23,7 @@ function Categories({ days, tasks, removeTask, updateTask }) {
                 })}
             </div>
             <div className="col s10 teal">
-                {isTrue ? <h4 className="center">A daily task planner to keep all your ducks in a row!</h4> : null}
+                {isTrue ? <div><h4 className="center task-header">A daily task planner to keep all your ducks in a row!</h4><h4 className="center">🦆🦆🦆🦆🦆🦆🦆</h4></div> : null}
                 {tasks.map(task => {
                     if(task.day_id === day.id){
                         return <TaskCard key={task.id} task={task} removeTask={removeTask} updateTask={updateTask} />
